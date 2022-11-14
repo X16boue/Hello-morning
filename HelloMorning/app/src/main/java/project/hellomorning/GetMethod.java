eed7e5a832c7fec823663a6dbfd11855e234439c
x16boue <xavier.sesboue@gmail.com>
2022-11-15 00:45:53 +0900

    Name of the city asked as parameter of getData method

--- HelloMorning/app/src/main/java/project/hellomorning/GetMethod.java
+++ HelloMorning/app/src/main/java/project/hellomorning/GetMethod.java
@@ -9,13 +9,14 @@ import java.net.URL;
 
 public class GetMethod {
 
-    public static String getData() {
+    public static String getData(String city) {
         BufferedReader in = null;
         String data = null;
         HttpURLConnection urlConnection = null;
 
         try {
-            URL url = new URL("https://api.openweathermap.org/data/2.5/weather?q=Seoul&appid=35eabfbc88074474775d676e2f0fc2ef");
+            String stringUrl = "https://api.openweathermap.org/data/2.5/weather?q="+city+"&appid=35eabfbc88074474775d676e2f0fc2ef";
+            URL url = new URL(stringUrl);
             urlConnection = (HttpURLConnection) url.openConnection();
             in = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
             StringBuffer sb = new StringBuffer("");

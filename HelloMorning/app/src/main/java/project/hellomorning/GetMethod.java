b19de14baa2d2e08148112cc166143aeb50b3b5d
x16boue <xavier.sesboue@gmail.com>
2022-11-14 21:38:03 +0900

    Less useless imports

--- HelloMorning/app/src/main/java/project/hellomorning/GetMethod.java
+++ HelloMorning/app/src/main/java/project/hellomorning/GetMethod.java
@@ -2,18 +2,9 @@ package project.hellomorning;
 
 import android.util.Log;
 
-import com.google.firebase.crashlytics.buildtools.reloc.org.apache.http.HttpResponse;
-import com.google.firebase.crashlytics.buildtools.reloc.org.apache.http.client.HttpClient;
-import com.google.firebase.crashlytics.buildtools.reloc.org.apache.http.client.methods.HttpGet;
-import com.google.firebase.crashlytics.buildtools.reloc.org.apache.http.conn.scheme.Scheme;
-import com.google.firebase.crashlytics.buildtools.reloc.org.apache.http.impl.client.DefaultHttpClient;
-
-import java.io.BufferedInputStream;
 import java.io.BufferedReader;
-import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
-import java.net.URI;
 import java.net.URL;
 
 public class GetMethod {

17264ec20f46be1b4c3f935c5b24d2535ed1a1e5
x16boue <xavier.sesboue@gmail.com>
2022-11-15 01:30:13 +0900

    Clearing of warnings

--- HelloMorning/app/src/main/java/project/hellomorning/GetMethod.java
+++ HelloMorning/app/src/main/java/project/hellomorning/GetMethod.java
@@ -10,13 +10,12 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.text.DecimalFormat;
 
 public class GetMethod {
 
 
     public static String getData(String city) {
-        BufferedReader in = null;
+        BufferedReader in;
         String data = null;
         HttpURLConnection urlConnection = null;
 
@@ -25,15 +24,14 @@ public class GetMethod {
             URL url = new URL(stringUrl);
             urlConnection = (HttpURLConnection) url.openConnection();
             in = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
-            StringBuffer sb = new StringBuffer("");
-            String l = "";
+            StringBuilder sb = new StringBuilder();
+            String l;
             String nl = System.getProperty("line.separator");
             while ((l = in.readLine()) != null) {
-                sb.append(l + nl);
+                sb.append(l).append(nl);
             }
             in.close();
             data = sb.toString();
-            Log.w("", data);
         } catch (Exception e) {
             e.printStackTrace();
         } finally {
@@ -91,40 +89,6 @@ public class GetMethod {
             e.printStackTrace();
         }
         return description;
-    }
-
-
-
-    public static String getSumUp(String response) {
-        DecimalFormat df = new DecimalFormat("#.##");
-
-        String output = "";
-        try {
-            JSONObject jsonResponse = new JSONObject(response);
-            JSONArray jsonArray = jsonResponse.getJSONArray("weather");
-            //JSONObject jsonObjectWeather = jsonArray.getJSONObject(0);
-            JSONObject jsonObjectMain = jsonResponse.getJSONObject("main");
-            double temp = jsonObjectMain.getDouble("temp") - 273.15;
-            double feelsLike = jsonObjectMain.getDouble("feels_like") - 273.15;
-            int humidity = jsonObjectMain.getInt("humidity");
-            JSONObject jsonObjectWind = jsonResponse.getJSONObject("wind");
-            String wind = jsonObjectWind.getString("speed");
-            JSONObject jsonObjectClouds = jsonResponse.getJSONObject("clouds");
-            String clouds = jsonObjectClouds.getString("all");
-            JSONObject jsonObjectSys = jsonResponse.getJSONObject("sys");
-            String cityName = jsonResponse.getString("name");
-
-            output += "Current weather of " + cityName
-                    + "\n Temp: " + df.format(temp) + " °C"
-                    + "\n Feels Like: " + df.format(feelsLike) + " °C"
-                    + "\n Humidity: " + humidity + "%"
-                    + "\n Wind Speed: " + wind + "m/s (meters per second)"
-                    + "\n Cloudiness: " + clouds + "%";
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return output;
     }
 
 }
--- HelloMorning/app/src/main/java/project/hellomorning/MainActivity.java
+++ HelloMorning/app/src/main/java/project/hellomorning/MainActivity.java
@@ -6,9 +6,6 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.StrictMode;
 import android.util.Log;
-import android.view.View;
-import android.widget.Button;
-import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,19 +17,14 @@ public class MainActivity extends AppCompatActivity {
         StrictMode.setThreadPolicy(policy);
 
 
-        ((Button)findViewById(R.id.jumpToForm)).setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v){
-                Intent intent = new Intent(MainActivity.this, FormActivity.class);
-                startActivity(intent);
-            }
+        (findViewById(R.id.jumpToForm)).setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, FormActivity.class);
+            startActivity(intent);
         });
 
-        ((Button)findViewById(R.id.APItest)).setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v){
-                String result =  GetMethod.getData("Pohang");
-            }
+        (findViewById(R.id.APItest)).setOnClickListener(v -> {
+            String result =  GetMethod.getData("Pohang");
+            Log.w("API results", result);
         });
     }
 }

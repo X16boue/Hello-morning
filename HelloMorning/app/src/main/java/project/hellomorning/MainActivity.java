977a0006ca1820e995d259609202df31b67741ac
x16boue <xavier.sesboue@gmail.com>
2022-11-27 23:37:04 +0900

    Reading of the file containing the answers to the form. The data is loaded as an array list named 'material'

--- HelloMorning/app/src/main/java/project/hellomorning/MainActivity.java
+++ HelloMorning/app/src/main/java/project/hellomorning/MainActivity.java
@@ -2,10 +2,21 @@ package project.hellomorning;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,6 +24,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        String filename = "userdata";
+
+        List<String> materials = fetchMaterialFromFile(filename);
+        Log.w("", String.valueOf(materials.size()));
 
 
         ((Button)findViewById(R.id.jumpToForm)).setOnClickListener(new View.OnClickListener(){
@@ -22,5 +37,19 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+    }
+
+    protected List<String> fetchMaterialFromFile(String filename){
+        Context context = getApplicationContext();
+        ArrayList<String> output = new ArrayList<>();
+        try (FileInputStream fis = context.openFileInput(filename)){
+            ObjectInputStream ois = new ObjectInputStream(fis);
+            output = (ArrayList<String>) ois.readObject();
+            ois.close();
+            Toast.makeText(getApplicationContext(),"Materials loaded", Toast.LENGTH_SHORT).show();
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+        return output;
     }
 }
